2345361b11ef73277241ad6177f25034e9e301ec
pravin khandagale <khandagale.pravin5@yahoo.in>
2018-06-15 01:54:05 +0530

    intial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const pincode = document.querySelector('#zipForm')
+let outputInfo = document.querySelector('#output');
+pincode.addEventListener('submit', runEvent);;
+
+function runEvent(e){
+  const zip = document.querySelector('.pincode').value;
+    axios.get(`http://api.zippopotam.us/IN/${zip}`)
+    .then(function (response){
+     if(response.status == 200 ){
+      response.data.places.forEach(place => {
+        outputInfo.innerHTML = `
+        <h4><strong>Country:</strong> ${response.data.country}</h4>
+        <h5><strong>Place:</strong> ${place['place name']}</h5>
+        <h5><strong>Longitude:</strong> ${place['longitude']}</h5>
+        <h5><strong>Latitude:</strong> ${place['latitude']}</h5>
+        `;
+        console.log(response.data);
+      })
+    
+      } else { 
+        outputInfo.innerHTML = `<p>Invaild Pincode, Please Try Again!<p>`;
+        // let output = '';
+          // response.data.places.forEach(place => {
+          //   outputInfo.innerHTML = `
+          //   <h4><strong>Country:</strong> ${response.data.country}</h4>
+          //   <h5><strong>Place:</strong> ${place['place name']}</h5>
+          //   <h5><strong>Longitude:</strong> ${place['longitude']}</h5>
+          //   <h5><strong>Latitude:</strong> ${place['latitude']}</h5>
+          //   `;
+          //   console.log(response.data);
+          // })
+      }
+    })
+    e.preventDefault();
+}
